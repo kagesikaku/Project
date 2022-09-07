55a24f97524947dcc3392e7f8d2870cc280fc06f
shreyamadiga <shreyamadiga@gmail.com>
2022-09-07 22:19:43 +0000

    k

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :articles
+  root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
